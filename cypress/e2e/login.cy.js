e89f892454172d8354a2cde56f170f47a3ce5d59
anushila9 <anushila.basnet@gmail.com>
2026-06-17 17:19:03 +0545

    added login and dashboard pages with cypress login tests

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,27 @@
+describe('Hospital Login Page', ()=> {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/login.html')
+    })
+
+    it('should load the login page', () => {
+        cy.get('h2').should('contain', 'Patient Login');
+    })
+
+    it('should show error message for blank input in credentials section', () => {
+        cy.get('#loginBtn').click()
+        cy.get('#errorMessage').should('contain', 'Please fill in all fields.')
+    })
+
+    it('should show error for invalid message', () => {
+        cy.get('#email').type('wrongemail@gmail.com')
+        cy.get('#password').type('wrongpassword')
+        cy.get('#loginBtn').click()
+        cy.get('#errorMessage').should('contain', 'Invalid email or password')
+    })
+    it('should login successfully with valid credentials', () => {
+        cy.get('#email').type('patient@medicare.com')
+        cy.get('#password').type('Test@1234')
+        cy.get('#loginBtn').click()
+        cy.url().should('include', 'dashboard.html')
+    })
+})
